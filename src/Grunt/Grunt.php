3facd1627785f06d113d51106af5842f0211ccaa
Taufan Aditya <toopay@taufanaditya.com>
2012-10-11 15:33:02 +0700

    Inspect phpunit dep

--- src/Grunt/Grunt.php
+++ src/Grunt/Grunt.php
@@ -74,8 +74,10 @@ class Grunt {
 			$fileName = str_replace($candidate . DIRECTORY_SEPARATOR, __DIR__ . DIRECTORY_SEPARATOR, $fileName);
 		}
 
-		if(file_exist($fileName)) {
+		if(file_exists($fileName)) {
 			require_once $fileName;
+
+			return;
 		}
 
 		return FALSE;
